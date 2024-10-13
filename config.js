2bf1539424b88911383e887025a7218ede15f5f1
Menu11 <khongking50@gmail.com>
2024-10-13 07:45:24 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+module.exports = {
+SESSION_ID: process.env.SESSION_ID || "add youe session id here",
+MONGODB: process.env.MONGODB || "your mongodb uri here",
+};
